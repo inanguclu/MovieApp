dcbf596ea5df38ee0072e24381353ecb250ba2f9
İnan güçlü <77547694+inanguclu@users.noreply.github.com>
2023-10-27 17:24:25 +0300

    Dashboard_View

--- src/app/movie.datasource.ts
+++ src/app/movie.datasource.ts
@@ -2,7 +2,11 @@ import { Movie } from "./movie";
 
 
 export const Movies:Movie[]=[
-    {id:1,name:"movie 1"},
-    {id:2,name:"movie 2"},
-    {id:3,name:"movie 3"}
+    {id:1,name:"movie 1",description:"bu film harika",imageUrl:""},
+    {id:2,name:"movie 2",description:"bu film harika",imageUrl:""},
+    {id:3,name:"movie 3",description:"bu film harika",imageUrl:""},
+    {id:4,name:"movie 4",description:"bu film harika",imageUrl:""},
+    {id:5,name:"movie 5",description:"bu film harika",imageUrl:""},
+    {id:6,name:"movie 6",description:"bu film harika",imageUrl:""},
+    {id:7,name:"movie 7",description:"bu film harika",imageUrl:""}
 ];
