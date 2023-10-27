69b1f69475a1d928d08120b3931013db92b3f638
İnan güçlü <77547694+inanguclu@users.noreply.github.com>
2023-10-27 14:04:26 +0300

    Logging

---
+++ src/app/logging/logging.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { LoggingService } from '../logging.service';
+
+@Component({
+  selector: 'logging',
+  templateUrl: './logging.component.html',
+  styleUrls: ['./logging.component.css']
+})
+export class LoggingComponent {
+  constructor(public loggingService:LoggingService){
+
+  }
+  ngOnInit(){
+
+  }
+
+}
